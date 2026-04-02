07d969dfec2693d12be81e5ba04fda5bba0157f7
zaralauren00788 <zaralauren007@gmail.com>
2026-04-03 03:41:30 +0700

    app.py

--- app.py
+++ app.py
@@ -69,3 +69,14 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+    import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
